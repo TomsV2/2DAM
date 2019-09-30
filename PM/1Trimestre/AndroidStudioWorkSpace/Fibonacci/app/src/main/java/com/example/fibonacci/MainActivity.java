64d2dfa537b68e0e0941a294382c22ff465173e9
Tomás Núñez <tomasmateo98@gmail.com>
2019-09-30 23:24:22 +0200

    .

--- PM/1Trimestre/AndroidStudioWorkSpace/Fibonacci/app/src/main/java/com/example/fibonacci/MainActivity.java
+++ PM/1Trimestre/AndroidStudioWorkSpace/Fibonacci/app/src/main/java/com/example/fibonacci/MainActivity.java
@@ -10,23 +10,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button boton;
     TextView sFibonacci;
     Integer iFibonacci;
-    int antFibonacci = 0;
+    int antFibonacci = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void calcularFibonacci(View view) {
+        boton = (Button) findViewById(R.id.boton);
         sFibonacci = (TextView) findViewById(R.id.fibonacci);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, (CharSequence) sFibonacci, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /*sFibonacci = (TextView) findViewById(R.id.fibonacci);
 
         iFibonacci = Integer.parseInt(String.valueOf(sFibonacci));
 
         iFibonacci = antFibonacci + iFibonacci;
 
-        sFibonacci.append(String.valueOf(iFibonacci));
+        Toast.makeText(MainActivity.this, String.valueOf(iFibonacci), Toast.LENGTH_LONG).show();*/
+
+        //sFibonacci.append(String.valueOf(iFibonacci));
+
     }
 }
