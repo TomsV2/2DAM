34b48d349896c4abf4b60f1dbdaadbee34ec22f5
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2019-10-16 12:46:03 +0200

    PM - Fragmentos

---
+++ PM/1Trimestre/AndroidStudioWorkSpace/Fragment_List/app/src/main/java/com/example/fragment_list/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.fragment_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contenedor, ContactosFragment.newInstance(2))
+                .commit();
+
+    }
+}
