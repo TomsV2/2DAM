76b1915cf4184c02ae44d254248f9f4c9d3c2a52
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2019-10-17 12:43:46 +0200

    PM

---
+++ PM/1Trimestre/AndroidStudioWorkSpace/Multipantallas_Tablets/app/src/main/java/com/example/multipantallas_tablets/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.multipantallas_tablets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
