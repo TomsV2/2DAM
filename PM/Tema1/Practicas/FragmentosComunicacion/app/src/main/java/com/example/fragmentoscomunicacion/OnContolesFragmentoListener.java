08b8660a82ef26434ab088b552d5223b7ad2de1f
Tomás Núñez <tomasmateo98@gmail.com>
2019-10-28 23:49:54 +0100

    .

---
+++ PM/Tema1/Practicas/FragmentosComunicacion/app/src/main/java/com/example/fragmentoscomunicacion/OnContolesFragmentoListener.java
@@ -0,0 +1,6 @@
+package com.example.fragmentoscomunicacion;
+
+public interface OnContolesFragmentoListener {
+    public void botonCambioColorClick(String color);
+    public void botonCambioTextoClick(String texto);
+}
