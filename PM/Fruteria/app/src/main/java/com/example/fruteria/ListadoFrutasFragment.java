217e5f8470c9af2228d74e77f924c793dd90b197
Tomás Mateo Núñez Ríos <tomasmateo98@gmail.com>
2019-11-14 10:16:23 +0100

    PM - Frutería
    Empezando ejercicio de la frutería

---
+++ PM/Fruteria/app/src/main/java/com/example/fruteria/ListadoFrutasFragment.java
@@ -0,0 +1,31 @@
+package com.example.fruteria;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListadoFrutasFragment extends Fragment {
+
+
+    public ListadoFrutasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_listado_frutas, container, false);
+    }
+
+}
--- PM/Fruteria/app/src/main/java/com/example/fruteria/MainActivity.java
+++ PM/Fruteria/app/src/main/java/com/example/fruteria/MainActivity.java
@@ -29,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -45,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send,R.id.nav_listado)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
