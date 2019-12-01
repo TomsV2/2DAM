1f3de092b90e70078ed7fbf71157d09eeae4b995
Tomás Núñez <tomasmateo98@gmail.com>
2019-12-01 21:45:41 +0100

    .

---
+++ AD/ProyectoAD/src/Clases/Arbol.java
@@ -0,0 +1,15 @@
+package Clases;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Arbol {
+
+    //Atributos
+    HashMap<String, Integer> nodoRaiz = new HashMap<>();
+
+    HashMap<String, Integer> nodoIzq = new HashMap<>();
+    HashMap<String, Integer> nodoDer = new HashMap<>();
+
+
+}
