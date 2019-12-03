d2d373735eb608a55f408831cb9805d7575b0b77
Tomás Núñez <tomasmateo98@gmail.com>
2019-12-03 03:25:42 +0100

    Morir quiero

---
+++ AD/ProyectoAD/src/Gestoras/GestoraProyectoAD.java
@@ -0,0 +1,365 @@
+package Gestoras;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class GestoraProyectoAD {
+
+    public String encriptar(String texto, String [][] tablaNunez, ArrayList<String> arrayLetras){
+
+        String textoBinario = "";
+
+        for(char dato:texto.toCharArray()){
+            System.out.print(dato);
+            //System.out.println(arrayLetras.indexOf(String.valueOf(dato)));
+            int posicionDato = arrayLetras.indexOf(String.valueOf(dato));
+
+            for(int j=0; j<=tablaNunez[posicionDato].length-1; j++){
+                if(!tablaNunez[posicionDato][j].equals(" ")) {
+                    textoBinario = textoBinario + tablaNunez[posicionDato][j];
+                }
+            }
+            System.out.println();
+        }
+
+        /*for(int i=0; i<=tablaNunez.length-1; i++){ //Cantidad de filas
+
+            //textoBinario = "";
+
+            for(int j=0; j<=tablaNunez[i].length-1; j++){   //Cantidad de columnas en la fila i
+                if(!tablaNunez[i][j].equals(" ")){
+                    textoBinario = textoBinario + tablaNunez[i][j];
+                }
+            }
+        }*/
+
+        return textoBinario;
+    }
+
+    public void cargarArrays(String texto, ArrayList<String> arrayLetras, ArrayList<Integer> arrayCantidades){
+
+        int posicionLetra;
+
+        for(int i=0; i<texto.length(); i++){
+
+            String letra = String.valueOf(texto.charAt(i));
+
+            //System.out.println(letra);
+
+            if( arrayLetras.contains(letra) == false ) {
+                arrayLetras.add(letra);
+                arrayCantidades.add(1);
+            }
+            else{
+                posicionLetra = arrayLetras.indexOf(letra);
+                arrayCantidades.set( posicionLetra, arrayCantidades.get(posicionLetra)+1 );
+            }
+        }
+    }
+
+    public String crearTraductor(ArrayList arrayLetras){
+
+        int paresBinarios;
+
+        if(arrayLetras.size()%2==0){
+            paresBinarios = arrayLetras.size();
+        }
+        else{
+            paresBinarios = arrayLetras.size()+1;
+        }
+
+        String [][] tablaNunez = new String[arrayLetras.size()][paresBinarios];
+
+        for(int i=0; i<=arrayLetras.size()-1; i++){
+            for(int j=0; j<=paresBinarios-1; j++){
+                tablaNunez[i][j] = " ";
+            }
+        }
+
+
+        int intervalo = 1;
+        int c = 1;
+        boolean banderaNumero;
+
+        for(int j=0; j<=paresBinarios-1; j++){
+
+            banderaNumero = true;
+
+            if(j%2==0){
+                for(int i=0; i<=arrayLetras.size()-1; i++){
+
+                    if(banderaNumero == true){
+                        if(c <= intervalo){
+                            tablaNunez[i][j] = "0";
+
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = false;
+                                c = 1;
+                            }
+                        }
+                    }
+                    else{
+                        if(c <= intervalo){
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = true;
+                                c = 1;
+                            }
+                        }
+                    }
+
+                }
+            }
+            else{
+                c = 1;
+                for(int i=intervalo; i<=arrayLetras.size()-1; i++){
+
+                    if(banderaNumero == true){
+                        if(c <= intervalo){
+                            tablaNunez[i][j] = "1";
+
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = false;
+                                c = 1;
+                            }
+                        }
+                    }
+                    else{
+                        if(c <= intervalo){
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = true;
+                                c = 1;
+                            }
+                        }
+                    }
+
+                }
+                intervalo++;
+                c = 1;
+            }
+        }
+
+        String cifradoBinario = "";
+
+        for(int i=0; i<=arrayLetras.size()-1; i++){
+
+            cifradoBinario = "";
+
+            System.out.print(arrayLetras.get(i) +":");
+
+            for(int j=0; j<=paresBinarios-1; j++){
+                if(!tablaNunez[i][j].equals(" ")){
+                    cifradoBinario = cifradoBinario + tablaNunez[i][j];
+                }
+            }
+
+            System.out.print(cifradoBinario);
+            System.out.print(".");
+        }
+        return cifradoBinario;
+    }
+
+    public String[][] crearTabla(ArrayList arrayLetras){
+
+        int paresBinarios;
+
+        if(arrayLetras.size()%2==0){
+            paresBinarios = arrayLetras.size();
+        }
+        else{
+            paresBinarios = arrayLetras.size()+1;
+        }
+
+        String [][] tablaNunez = new String[arrayLetras.size()][paresBinarios];
+
+        for(int i=0; i<=arrayLetras.size()-1; i++){
+            for(int j=0; j<=paresBinarios-1; j++){
+                tablaNunez[i][j] = " ";
+            }
+        }
+
+
+        int intervalo = 1;
+        int c = 1;
+        boolean banderaNumero;
+
+        for(int j=0; j<=paresBinarios-1; j++){
+
+            banderaNumero = true;
+
+            if(j%2==0){
+                for(int i=0; i<=arrayLetras.size()-1; i++){
+
+                    if(banderaNumero == true){
+                        if(c <= intervalo){
+                            tablaNunez[i][j] = "0";
+
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = false;
+                                c = 1;
+                            }
+                        }
+                    }
+                    else{
+                        if(c <= intervalo){
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = true;
+                                c = 1;
+                            }
+                        }
+                    }
+
+                }
+            }
+            else{
+                c = 1;
+                for(int i=intervalo; i<=arrayLetras.size()-1; i++){
+
+                    if(banderaNumero == true){
+                        if(c <= intervalo){
+                            tablaNunez[i][j] = "1";
+
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = false;
+                                c = 1;
+                            }
+                        }
+                    }
+                    else{
+                        if(c <= intervalo){
+                            c++;
+
+                            if(c>intervalo){
+                                banderaNumero = true;
+                                c = 1;
+                            }
+                        }
+                    }
+
+                }
+                intervalo++;
+                c = 1;
+            }
+        }
+
+        /*for(int i=0; i<=letras.size()-1; i++){
+            for(int j=0; j<=paresBinarios-1; j++){
+                System.out.print(tablaNunez[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();*/
+
+        String cifradoBinario;
+
+        for(int i=0; i<=arrayLetras.size()-1; i++){
+
+            cifradoBinario = "";
+
+            System.out.print(arrayLetras.get(i) +":");
+
+            for(int j=0; j<=paresBinarios-1; j++){
+                if(!tablaNunez[i][j].equals(" ")){
+                    cifradoBinario = cifradoBinario + tablaNunez[i][j];
+                }
+            }
+
+            System.out.print(cifradoBinario);
+            System.out.print(".");
+        }
+        return tablaNunez;
+    }
+
+    public void MetodoBurbuja(ArrayList<String> arrayLetras, ArrayList<Integer> arrayCantidades){
+
+        int n = arrayCantidades.size();
+        int auxCantidades = 0;
+        String auxLetras = "";
+
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arrayCantidades.get(j-1) > arrayCantidades.get(j)){
+
+                    //Intercambiar elementos
+                    auxCantidades = arrayCantidades.get(j-1);
+                    auxLetras = arrayLetras.get(j-1);
+
+                    arrayCantidades.set(j-1, arrayCantidades.get(j));
+                    arrayLetras.set(j-1, arrayLetras.get(j));
+
+                    arrayCantidades.set(j, auxCantidades);
+                    arrayLetras.set(j, auxLetras);
+
+                }
+            }
+        }
+
+        //return arrayLetras;
+    }
+
+    public String LeerFichero(){
+
+        String texto = "";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/Ficheros/Texto.txt"));
+
+            String linea = br.readLine();
+
+            while (linea != null) {
+                texto = texto+linea;
+
+                linea = br.readLine();
+            }
+
+            br.close();
+
+            //System.out.println(texto);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return texto;
+    }
+
+    public byte[] getBytesFromFile(String texto) throws IOException {
+        long length = texto.length();
+
+        if (length > Integer.MAX_VALUE) {
+            throw new IOException("El archivo es kilometrico!!! Intente uno mas chico.");
+        }
+
+        byte[] bytes = new byte[(int)length];
+        int offset = 0;
+        int numRead = 0;
+
+        texto.getBytes();
+
+        InputStream is = new FileInputStream(texto);
+        try {
+            while (offset < bytes.length
+                    && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+                offset += numRead;
+            }
+        } finally {
+            is.close();
+        }
+
+        return bytes;
+    }
+}
--- AD/ProyectoAD/src/Main/MainProyectoAD.java
+++ AD/ProyectoAD/src/Main/MainProyectoAD.java
@@ -1,127 +1,87 @@
 package Main;
 
+import Gestoras.GestoraProyectoAD;
+
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 public class MainProyectoAD {
 
-    public static byte[] getBytesFromFile(String texto) throws IOException {
-        long length = texto.length();
 
-        if (length > Integer.MAX_VALUE) {
-            throw new IOException("El archivo es kilometrico!!! Intente uno mas chico.");
-        }
-
-        byte[] bytes = new byte[(int)length];
-        int offset = 0;
-        int numRead = 0;
-
-        texto.getBytes();
-
-        InputStream is = new FileInputStream(texto);
-        try {
-            while (offset < bytes.length
-                    && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
-                offset += numRead;
-            }
-        } finally {
-            is.close();
-        }
-
-        return bytes;
-    }
 
     public static void main(String [] args){
 
-        int posicionLetra = 0;
+        //Declaraciones de variables
+        int posicionLetra;
         String texto = "";
+        int paresBinarios;
+        String traductor = "";
 
-        File ficheroBin = new File("src/Ficheros/binario.bin");
-        File txt = new File("src/Ficheros/Texto.txt");
+        //Inicializaciones
+        GestoraProyectoAD gestora = new GestoraProyectoAD();
+
+        File ficheroEncriptado = new File("src/Ficheros/x.txt");
+        File ficheroTraductor = new File("src/Ficheros/traductor.txt");
 
         ArrayList<String> letras = new ArrayList<String>();
         ArrayList<Integer> cantidades = new ArrayList<Integer>();
 
+        String[][] tablaNunez;
+
         //Leer texto del fichero .txt
+        texto = gestora.LeerFichero();
+
+        //Cargar arrays
+        gestora.cargarArrays(texto, letras, cantidades);
+
+        //Ordenar las palabras por frecuencia (menor a mayor)
+        gestora.MetodoBurbuja(letras, cantidades);
+
+        //Crear codificación de las letras del texto
+        tablaNunez = gestora.crearTabla(letras);
+
+        System.out.println();
+
+        System.out.println(gestora.encriptar(texto, tablaNunez, letras));
+
         try {
-            BufferedReader br = new BufferedReader(new FileReader("src/Ficheros/Texto.txt"));
+            ficheroEncriptado.createNewFile();
 
-            String linea = br.readLine();
+            FileWriter fw = new FileWriter(ficheroEncriptado.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
 
-            while (linea != null) {
-                texto = texto+linea;
+            bw.write(gestora.encriptar(texto, tablaNunez, letras).toCharArray());
 
-                linea = br.readLine();
-            }
-
-            br.close();
-
-            System.out.println(texto);
-
+            bw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for(int i=0; i<texto.length(); i++){
+        try {
+            ficheroTraductor.createNewFile();
 
-            String letra = String.valueOf(texto.charAt(i));
+            FileWriter fw = new FileWriter(ficheroTraductor.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
 
-            System.out.println(letra);
+            bw.write(gestora.crearTraductor(letras));
 
-            if( letras.contains(letra) == false ) {
-                letras.add(letra);
-                cantidades.add(1);
-            }
-            else{
-                posicionLetra = letras.indexOf(letra);
-                cantidades.set( posicionLetra, cantidades.get(posicionLetra)+1 );
-            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        System.out.println();
-
-        for(String letra:letras){
-            System.out.println(letra);
-        }
-        System.out.println();
-        for(Integer cantidad:cantidades){
-            System.out.println(cantidad);
-        }
-
-        int n = cantidades.size();
-        int auxCantidades = 0;
-        String auxLetras = " ";
-
-        for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(cantidades.get(j-1) > cantidades.get(j)){
-
-                    //Intercambiar elementos
-                    auxCantidades = cantidades.get(j-1);
-                    auxLetras = letras.get(j-1);
-
-                    cantidades.set(j-1, cantidades.get(j));
-                    letras.set(j-1, letras.get(j));
-
-                    cantidades.set(j, auxCantidades);
-                    letras.set(j, auxLetras);
-
-                }
-            }
-        }
-
-        System.out.println();
-
-        for(String letra:letras){
-            System.out.println(letra);
-        }
-        System.out.println();
-        for(Integer cantidad:cantidades){
-            System.out.println(cantidad);
-        }
-
-
+        System.out.println(traductor);
 
     }
 }
+
+/*
+for(String letra:letras){
+    System.out.println(letra);
+}
+System.out.println();
+for(Integer cantidad:cantidades){
+    System.out.println(cantidad);
+}
+*/
